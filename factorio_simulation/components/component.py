122abe10f837018be87da9e85ab3d807ef16755a
Daniel Barton <daniel.barton446@gmail.com>
2024-01-14 19:43:14 -0500

    feat/fix: random names & typo component_id

--- factorio_simulation/components/component.py
+++ factorio_simulation/components/component.py
@@ -3,7 +3,7 @@
 class Component:
 
     def __init__(self, component_id):
-        self.entity_id = component_id
+        self.component_id = component_id
 
     def __hash__(self):
         return hash(type(self))
--- factorio_simulation/components/dummy_component.py
+++ factorio_simulation/components/dummy_component.py
@@ -1,6 +1,8 @@
 from factorio_simulation.components.component import Component
+import randomname
+
 
 class DummyComponent(Component):
     def __init__(self, component_id):
         super().__init__(component_id)
-        self.name = "DummyComponent"
+        self.name = randomname.generate('names/surnames/english')
