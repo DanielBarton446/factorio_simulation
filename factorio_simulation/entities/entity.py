019ed1ac32aa298abf25da887826c7ed15561290
Daniel Barton <daniel.barton446@gmail.com>
2024-01-14 19:44:15 -0500

    feat: dict based entity system instead of poor set

--- factorio_simulation/entities/entity.py
+++ factorio_simulation/entities/entity.py
@@ -1,30 +1,44 @@
 from factorio_simulation.components.component import Component
-from typing import Set, Optional
+from typing import Dict, Optional
 
 
 class Entity:
+    """
+    An entity is a collection of components.
+    """
 
     def __init__(self, entity_id):
+        """
+        @param entity_id: The unique id of the entity
+        @param components: Set of components that the entity has
+        """
+
         self.entity_id: int = entity_id
-        self.components: Set[Component] = set()
+        self.components: Dict = dict()
 
     def add_component(self, component: Component):
-        # we will make it so that Component has a hash 
-        # function that returns the component type
-        self.components.add(component)
+        """
+        Add a component to the entity
+        """
+        if self.components.get(type(component)) is not None:
+            raise Exception("Entity already has a component of type: ",
+                            type(component))
+        self.components[type(component)] = component
 
-    # def remove_component(self, component_type: type):
-    #     self.components.remove(component_type)
+    def remove_component(self, component_type: type):
+        del self.components[component_type]
 
     def get_component(self, component_type: type) -> Optional[Component]:
-        for component in self.components:
-            if isinstance(component, component_type):
-                return component
-        return None
+        """
+        Given the type of component, return the component on the entity.
+        """
+        return self.components.get(component_type)
 
     def has_component_type(self, component_type: type) -> bool:
-        for component in self.components:
-            if type(component) is component_type:
-                return True
-            
-        return False
+        """
+        Given the type of component, return True if the entity
+        has the component.
+        """
+
+        return self.components.get(component_type) is not None
+
